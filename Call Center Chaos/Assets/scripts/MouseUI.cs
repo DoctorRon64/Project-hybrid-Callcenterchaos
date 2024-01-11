35c88c0c4db5ffe9299b0eb397015c51729ba3d0
GooForSupper <llvandersloot@gmail.com>
2024-01-11 15:59:09 +0100

    post processing

--- Call Center Chaos/Assets/scripts/MouseUI.cs
+++ Call Center Chaos/Assets/scripts/MouseUI.cs
@@ -6,12 +6,13 @@ public class MouseUI : MonoBehaviour
 {
     [SerializeField] public RectTransform UIObject;
 
+    Vector3 targetPosition;
     AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
-        audio = GetComponent<AudioSource>();
+        audio = GetComponent<AudioSource>();;
     }
 
     // Update is called once per frame
@@ -28,6 +29,9 @@ public class MouseUI : MonoBehaviour
 
     void MoveObjectToMouse()
     {
-        UIObject.position = Input.mousePosition;
+        //UIObject.position = Input.mousePosition;
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = 0;
+        transform.position = targetPosition;
     }
 }
